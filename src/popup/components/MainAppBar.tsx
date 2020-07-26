39b2f8760dc08dc3f7e3bd90065745f993f3be3c
Abner Zheng <abnerzheng@gmail.com>
2020-07-27 04:40:36 +0800

    ECO-487: add HomeIcon in the left of AppBar.

--- src/popup/components/MainAppBar.tsx
+++ src/popup/components/MainAppBar.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import MoreMenu from './Menu';
+import HomeIcon from '@material-ui/icons/Home';
 import AccountManager from '../container/AccountManager';
 import { observer } from 'mobx-react';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -37,8 +41,15 @@ export const MainAppBar = observer((props: Props) => {
   if (props.authContainer.hasCreatedVault && props.authContainer.isUnLocked) {
     return (
       <React.Fragment>
-        <AppBar position="fixed" className={classes.root} color={'transparent'}>
+        <AppBar
+          position="static"
+          className={classes.root}
+          color={'transparent'}
+        >
           <Toolbar>
+            <IconButton edge="start" component={Link} to={'/'}>
+              <HomeIcon />
+            </IconButton>
             <Typography variant="h6" className={classes.title}></Typography>
             <MoreMenu authContainer={props.authContainer} />
           </Toolbar>
