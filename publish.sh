9eb70427c2c0b9d1571188092e99ceb3f85a31ca
Piotr Skowron <piotr@putit.io>
2020-10-16 18:24:38 +0200

    debug cicd script

--- publish.sh
+++ publish.sh
@@ -1,6 +1,11 @@
 #!/bin/bash 
 
+ls -al /drone/src/artifacts/*zip
+
 ZIP_FILE=$(ls /drone/src/artifacts/*zip)
+
+echo $ZIP_FILE
+
 
 if [ ! -f $ZIP_FILE ]; then
   echo "[ERROR] No such file $ZIP_FILE"
@@ -9,6 +14,7 @@ fi
 
 ACCESS_TOKEN=$(curl https://accounts.google.com/o/oauth2/token -d "client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&refresh_token=${REFRESH_TOKEN}&grant_type=refresh_token&redirect_uri=urn:ietf:wg:oauth:2.0:oob" | jq -r .access_token)
 
+exit 0
 # upload
 UPLOAD_OUTPUT=$(curl --write-out %{http_code} -s -f -H "Authorization: Bearer ${ACCESS_TOKEN}" -H 'x-goog-api-version: 2' -X PUT -T $ZIP_FILE "https://www.googleapis.com/upload/chromewebstore/v1.1/items/${APPLICATION_ID}")
 
