23f70310964bebc54d95faa1b6016913c988cd5e
George <george@casperlabs.io>
2020-10-16 13:32:31 +0100

    [ECO-670] Fixes extension badge logic

--- src/background/utils.ts
+++ src/background/utils.ts
@@ -4,12 +4,10 @@ import { browser } from 'webextension-polyfill-ts';
 export function updateBadge(appState: AppState) {
   let label = '';
   let count = appState.toSignMessages.length;
-  let request = appState.connectionRequested;
-  if (count) {
-    label = String(count);
-  }
-  if (request) {
+  if (appState.connectionRequested) {
     label = '1';
+  } else if (count) {
+    label = String(count);
   }
   browser.browserAction.setBadgeText({ text: label });
   browser.browserAction.setBadgeBackgroundColor({ color: 'red' });
