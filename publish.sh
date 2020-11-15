b1729c7a77c5f502e4d922da078b40d1e8ca9f42
Piotr Skowron <piotr@putit.io>
2020-11-15 18:54:05 +0100

    remove exit

--- publish.sh
+++ publish.sh
@@ -32,7 +32,6 @@ fi
 
 ACCESS_TOKEN=$(curl https://accounts.google.com/o/oauth2/token -d "client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&refresh_token=${REFRESH_TOKEN}&grant_type=refresh_token&redirect_uri=urn:ietf:wg:oauth:2.0:oob" | jq -r .access_token)
 
-exit 0
 # upload
 echo "Going to upload: $ZIP_FILE"
 UPLOAD_OUTPUT=$(curl --write-out %{http_code} -s -f -H "Authorization: Bearer ${ACCESS_TOKEN}" -H 'x-goog-api-version: 2' -X PUT -T $ZIP_FILE "https://www.googleapis.com/upload/chromewebstore/v1.1/items/${APPLICATION_ID}")
