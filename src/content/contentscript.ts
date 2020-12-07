80b2ccb655ac1b6b436febab3c5eb74b1be6e1c4
George <george@casperlabs.io>
2020-12-07 17:54:39 +0000

    [ECO-783] Makes test methods available to window

--- src/content/contentscript.ts
+++ src/content/contentscript.ts
@@ -18,6 +18,20 @@ function injectCustomJs() {
   } catch (e) {
     console.error('CasperLabs provider injection failed.', e);
   }
+  try {
+    let jsPath = 'scripts/content/signerTestMethods.js';
+    const container = document.head || document.documentElement;
+    const scriptTag = document.createElement('script');
+    scriptTag.setAttribute('type', 'text/javascript');
+    scriptTag.src = browser.extension.getURL(jsPath);
+    container.insertBefore(scriptTag, container.children[0]);
+    scriptTag.onload = function () {
+      // remove after run the script
+      container.removeChild(scriptTag);
+    };
+  } catch (e) {
+    console.error('CasperLabs test provider injection failed.', e);
+  }
 }
 
 injectCustomJs();
