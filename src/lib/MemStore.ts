be3451ee8862b6726b368551fb730cc0569de006
George-CL <george@casperlabs.io>
2021-05-25 13:26:45 +0100

    Linter Formatting

--- src/lib/MemStore.ts
+++ src/lib/MemStore.ts
@@ -8,10 +8,8 @@ export class AppState {
   @observable connectionRequested: boolean = false;
   @observable hasCreatedVault: boolean = false;
   @observable selectedUserAccount: KeyPairWithAlias | null = null;
-  userAccounts: IObservableArray<KeyPairWithAlias> = observable.array<
-    KeyPairWithAlias
-  >([], { deep: true });
-  @observable unsignedDeploys: IObservableArray<
-    deployWithID
-  > = observable.array<deployWithID>([], { deep: true });
+  userAccounts: IObservableArray<KeyPairWithAlias> =
+    observable.array<KeyPairWithAlias>([], { deep: true });
+  @observable unsignedDeploys: IObservableArray<deployWithID> =
+    observable.array<deployWithID>([], { deep: true });
 }
