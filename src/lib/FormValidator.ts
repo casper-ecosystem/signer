704f4ad4000ccfa8bb9a179c9ab41b272fd558ed
George-CL <george@casperlabs.io>
2021-06-07 01:50:52 +0100

    Changes to use const and string types

--- src/lib/FormValidator.ts
+++ src/lib/FormValidator.ts
@@ -1,26 +1,26 @@
 import { FieldState } from 'formstate';
 import owasp from 'owasp-password-strength-test';
 
-owasp.tests.required.push((password: any) => {
-  let passwordString = password.toString() as String;
+owasp.tests.required.push((password: string) => {
+  const passwordString = password.toString() as String;
   if (passwordString.match(/correct\s*horse\s*battery\s*staple/i) !== null) {
     return 'Use your imagination not the example!';
   }
 });
 
-export const valueRequired = (val: any) => !val && 'Value required';
+export const valueRequired = (val: string) => !val && 'Value required';
 
-export const valuesMatch = (val1: any, val2: any) =>
+export const valuesMatch = (val1: string, val2: string) =>
   !(val1 === val2) && 'Passwords must match';
 
-export const isAlgorithm = (val: any) =>
+export const isAlgorithm = (val: string) =>
   !(val === 'ed25519' || val === 'secp256k1') && 'Invalid Algorithm';
 
-export const fieldSubmittable = (val: FieldState<any>) => {
+export const fieldSubmittable = (val: FieldState<string>) => {
   return val.hasBeenValidated && !val.hasError;
 };
 
-export const strongPassword = (val: any) => {
-  let result = owasp.test(val);
+export const strongPassword = (val: string) => {
+  const result = owasp.test(val);
   return !result.strong && result.errors[0];
 };
