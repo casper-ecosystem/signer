120d23b0c90836f80dd43ae9afe8c5b670c9911d
George-CL <george@casperlabs.io>
2021-06-22 14:59:00 +0100

    Stop watch script tracking changes in node_modules

--- watch.js
+++ watch.js
@@ -39,22 +39,27 @@ config.plugins.push(new ExtensionReloader());
 
 // Start Webpack in watch mode.
 const compiler = webpack(config);
-compiler.watch({}, function (err) {
-  if (err) {
-    console.error(err);
-  } else {
-    // Every time Webpack finishes recompiling copy all the assets of the
-    // "public" dir in the "build" dir (except for the index.html)
-    fs.copySync(paths.appPublic, paths.appBuild, {
-      dereference: true,
-      filter: file => file !== paths.appHtml
-    });
-    // Report on console the succesfull build
-    console.clear();
-    console.info(colors.green('Compiled successfully!'));
-    console.info('Built at', new Date().toLocaleTimeString());
-    console.info();
-    console.info('Note that the development build is not optimized.');
-    console.info('To create a production build, use npm build.');
+compiler.watch(
+  {
+    ignored: '**/node_modules/**'
+  },
+  function (err) {
+    if (err) {
+      console.error(err);
+    } else {
+      // Every time Webpack finishes recompiling copy all the assets of the
+      // "public" dir in the "build" dir (except for the index.html)
+      fs.copySync(paths.appPublic, paths.appBuild, {
+        dereference: true,
+        filter: file => file !== paths.appHtml
+      });
+      // Report on console the succesfull build
+      console.clear();
+      console.info(colors.green('Compiled successfully!'));
+      console.info('Built at', new Date().toLocaleTimeString());
+      console.info();
+      console.info('Note that the development build is not optimized.');
+      console.info('To create a production build, use npm build.');
+    }
   }
-});
+);
