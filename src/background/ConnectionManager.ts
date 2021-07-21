e5bce980851c0202cec3059523129b9c1de46fcf
George-CL <george@casperlabs.io>
2021-07-21 12:55:11 +0100

    Removes logic gate on backend as it breaks flow

--- src/background/ConnectionManager.ts
+++ src/background/ConnectionManager.ts
@@ -79,10 +79,6 @@ export default class ConnectionManager {
   }
 
   public requestConnection() {
-    if (this.appState.userAccounts.length === 0) {
-      this.popupManager.openPopup('noAccount');
-      return;
-    }
     this.appState.connectionRequested = true;
     this.popupManager.openPopup('connect');
   }
