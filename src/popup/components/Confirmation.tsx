fbdbe02297b2939f6c518f563f4a34e86c3c8139
George-CL <george@casperlabs.io>
2021-09-23 02:51:22 +0100

    Allow submission of confirmation dialogs with enter

--- src/popup/components/Confirmation.tsx
+++ src/popup/components/Confirmation.tsx
@@ -61,88 +61,96 @@ class Confirmation extends React.Component<Props, { boxChecked: boolean }> {
         aria-describedby="alert-dialog-description"
       >
         <DialogTitle id="alert-dialog-title">{this.props.title}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            {this.props.confirmation}
-          </DialogContentText>
-          {this.props.options.requirePassword && (
-            <FormControl>
-              <TextFieldWithFormState
-                autoFocus={true}
-                fieldState={
-                  this.accountManager.confirmPasswordForm.$.confirmPasswordField
-                }
-                required
-                label={'Password'}
-                type={'password'}
-              />
-            </FormControl>
-          )}
-          {this.props.options.requireCheckbox && (
-            <FormControlLabel
-              control={
-                <Checkbox
-                  style={{ alignSelf: 'flex-start' }}
-                  checked={this.state.boxChecked}
-                  onChange={this.handleCheckboxChange}
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+          }}
+        >
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              {this.props.confirmation}
+            </DialogContentText>
+            {this.props.options.requirePassword && (
+              <FormControl>
+                <TextFieldWithFormState
+                  autoFocus={true}
+                  fieldState={
+                    this.accountManager.confirmPasswordForm.$
+                      .confirmPasswordField
+                  }
+                  required
+                  label={'Password'}
+                  type={'password'}
                 />
-              }
-              label={this.props.options.checkboxText}
-              // Needs styling - the font's a bit big
-            />
-          )}
-        </DialogContent>
-        <DialogActions>
-          <Button
-            onClick={() => {
-              this.props.cancel();
-            }}
-            color="secondary"
-          >
-            {this.props.cancelLabel}
-          </Button>
-          {this.props.options.requirePassword ? (
-            <FormControl>
+              </FormControl>
+            )}
+            {this.props.options.requireCheckbox && (
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    style={{ alignSelf: 'flex-start' }}
+                    checked={this.state.boxChecked}
+                    onChange={this.handleCheckboxChange}
+                  />
+                }
+                label={this.props.options.checkboxText}
+                // Needs styling - the font's a bit big
+              />
+            )}
+          </DialogContent>
+          <DialogActions>
+            <Button
+              onClick={() => {
+                this.props.cancel();
+              }}
+              color="secondary"
+            >
+              {this.props.cancelLabel}
+            </Button>
+            {this.props.options.requirePassword ? (
+              <FormControl>
+                <Button
+                  type="submit"
+                  // TODO: Disable doesn't work - doesn't enable when field is non-null.
+                  // disabled={this.accountManager.confirmPasswordDisabled}
+                  onClick={async () => {
+                    let givenPassword =
+                      this.accountManager.confirmPasswordForm.$
+                        .confirmPasswordField.$;
+                    try {
+                      await this.accountManager.confirmPassword(givenPassword);
+                      this.accountManager.confirmPasswordForm.$.confirmPasswordField.reset();
+                      this.errors.dismissLast();
+                      this.props.proceed();
+                    } catch (e) {
+                      this.accountManager.confirmPasswordForm.$.confirmPasswordField.setError(
+                        e.message
+                      );
+                    }
+                  }}
+                  color="primary"
+                >
+                  {this.props.proceedLabel}
+                </Button>
+              </FormControl>
+            ) : (
               <Button
                 type="submit"
-                // TODO: Disable doesn't work - doesn't enable when field is non-null.
-                // disabled={this.accountManager.confirmPasswordDisabled}
-                onClick={async () => {
-                  let givenPassword =
-                    this.accountManager.confirmPasswordForm.$
-                      .confirmPasswordField.$;
-                  try {
-                    await this.accountManager.confirmPassword(givenPassword);
-                    this.accountManager.confirmPasswordForm.$.confirmPasswordField.reset();
-                    this.errors.dismissLast();
-                    this.props.proceed();
-                  } catch (e) {
-                    this.accountManager.confirmPasswordForm.$.confirmPasswordField.setError(
-                      e.message
-                    );
-                  }
+                disabled={
+                  this.props.options.requireCheckbox
+                    ? !this.state.boxChecked
+                    : false
+                }
+                onClick={() => {
+                  this.props.proceed();
                 }}
                 color="primary"
               >
                 {this.props.proceedLabel}
               </Button>
-            </FormControl>
-          ) : (
-            <Button
-              disabled={
-                this.props.options.requireCheckbox
-                  ? !this.state.boxChecked
-                  : false
-              }
-              onClick={() => {
-                this.props.proceed();
-              }}
-              color="primary"
-            >
-              {this.props.proceedLabel}
-            </Button>
-          )}
-        </DialogActions>
+            )}
+          </DialogActions>
+        </form>
       </Dialog>
     );
   }
